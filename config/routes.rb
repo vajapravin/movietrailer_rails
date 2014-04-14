7151e593c197205f9c75f59c785c827438b69bd0
Pravinkumar Vaja <pravinkumar.vaja@synechron.com>
2014-04-14 20:54:09 +0530

    header and footer devided

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 MovieTrailer::Application.routes.draw do
+  root 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
